ac8138c46bf6dfd430c006adb43be298a19965cc
DAWID <dk@dk.dk>
2025-11-02 21:38:50 +0100

    conf2

--- conftest.py
+++ conftest.py
@@ -3,11 +3,6 @@ import pytest
 def pytest_terminal_summary(terminalreporter, exitstatus):
     total = terminalreporter._numcollected
     passed = len(terminalreporter.stats.get("passed", []))
-    failed = len(terminalreporter.stats.get("failed", []))
-    skipped = len(terminalreporter.stats.get("skipped", []))
 
     terminalreporter.write_sep("-", "📊 PODSUMOWANIE TESTÓW 📊")
-    terminalreporter.write_line(f"✅ ZDANE TESTY: {passed}")
-    terminalreporter.write_line(f"❌ NIEZALICZONE TESTY: {failed}")
-    terminalreporter.write_line(f"⏭️ POMINIĘTE TESTY: {skipped}")
-    terminalreporter.write_line(f"📦 ŁĄCZNIE TESTÓW: {total}")
+    terminalreporter.write_line(f"✅ ZDANE TESTY: {passed}/{total} testów")
